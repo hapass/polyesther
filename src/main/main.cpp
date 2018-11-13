f06ce5ebb33f1bcd869bd81d3a5b4ae2850d8899
Pavel Zavoiskikh <hapass@ya.ru>
2018-11-13 03:07:33 +0300

    Star field demo.

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -7,9 +7,10 @@
 
 using namespace std;
 
-const int screen_width = 30;
-const int screen_height = 20;
-const char pixel = '*';
+//renderer
+const int screen_width = 100;
+const int screen_height = 100;
+const string pixel = "\u25A0";
 const string command = "\x1b[";
 const string color_palette_256 = "38;5;";
 const string set_color_command = "m";
@@ -56,22 +57,104 @@ void swap_buffers() {
 
         should_color_pixel = pixel_color != frame_buffer[i + 1];
     }
+    cout << reset_cursor();
 }
 
 void clear(unsigned short color) {
     std::fill(frame_buffer.begin(), frame_buffer.end(), color);
 }
 
+void draw_pixel(int x, int y, unsigned short color) {
+    int index = y * screen_width + x;
+    frame_buffer[index] = color;
+}
+
+//stars demo
+float star_speed = 0.01;
+int stars_count = 200;
+int fps = 25;
+
+vector<float> star_x(stars_count);
+vector<float> star_y(stars_count);
+vector<float> star_z(stars_count);
+
+float generate_random() {
+    return static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
+}
+
+void create_star(int i) {
+    star_x[i] = generate_random() * 2 - 1;
+    star_y[i] = generate_random() * 2 - 1;
+    star_z[i] = generate_random();
+}
+
+bool within_screen(int x, int y) {
+    return (0 <= x && x < screen_width) &&
+           (0 <= y && y < screen_height);
+}
+
+bool too_close(float z) {
+    return z <= 0;
+}
+
+void draw_stars(chrono::milliseconds delta) {
+    clear(0);
+
+    auto delta_seconds = chrono::duration_cast<chrono::seconds>(delta);
+    for(int i = 0; i < stars_count; i++) {
+        star_z[i] -= star_speed;
+        
+        float z = star_z[i];
+        if(too_close(z)) {
+            create_star(i);
+            continue;
+        }
+
+        int half_width = screen_width / 2;
+        int half_height = screen_height / 2;
+
+        int x = static_cast<int>((star_x[i]/z) * half_width + half_width);
+        int y = static_cast<int>((star_y[i]/z) * half_height + half_height);
+
+        if(!within_screen(x, y)) {
+            create_star(i);
+            continue;
+        }
+
+        draw_pixel(x, y, 17);
+    }
+
+    swap_buffers();
+}
+
 int main() {
     cout << '\n';
 
-    int color = 255;
-    while(color >= 0) {
-        clear(color);
-        swap_buffers();
-        cout << reset_cursor();
-        color--;
-        this_thread::sleep_for(20ms);
+    for(int i = 0; i < stars_count; i++) {
+        create_star(i);
+    }
+
+    bool isFinished = false;
+    auto frame_time = chrono::milliseconds(1000 / fps);
+    auto total_time = 0ms;
+
+    while(!isFinished) {
+        auto start = chrono::steady_clock::now();
+
+        draw_stars(frame_time);
+
+        auto finish = chrono::steady_clock::now();
+        auto time_left = frame_time - (finish - start);
+
+        if(time_left > 0ms) {
+            this_thread::sleep_for(time_left);
+        }
+
+        total_time += frame_time;
+
+        if(total_time > 30s) {
+            isFinished = true;
+        }
     }
 
     cout << command << 0 << set_color_command;
