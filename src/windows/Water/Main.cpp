a4b6f4145602410e68c0d65eb424c57fa891f027
Pavel Zavoiskikh <pavel.zavoiskikh@gmail.com>
2021-05-05 16:34:34 +0300

    Playing around with WinAPI to get it to render bitmap.

--- src/windows/Water/Main.cpp
+++ src/windows/Water/Main.cpp
@@ -1,31 +1,51 @@
 #include <Windows.h>
+#include <stdint.h>
+#include <cassert>
+
+static BITMAPINFO BackBufferInfo;
+static void* BackBuffer;
+
+#define OS_CALL(call) \
+    if (!(call)) \
+    { \
+         assert(false, "Windows API call failed."); \
+         exit(1); \
+    } \
+
+void CreateBackBuffer()
+{
+    OS_CALL(BackBuffer = VirtualAlloc(0, BackBufferInfo.bmiHeader.biWidth * BackBufferInfo.bmiHeader.biHeight * (BackBufferInfo.bmiHeader.biBitCount / CHAR_BIT), MEM_COMMIT, PAGE_READWRITE));
+}
+
+void DestroyBackBuffer()
+{
+    if (BackBuffer)
+    {
+        OS_CALL(VirtualFree(BackBuffer, 0, MEM_RELEASE));
+    }
+}
 
 LRESULT CALLBACK MainWindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
     switch (uMsg)
     {
-    case WM_CLOSE:
+    case WM_SIZE:
+    {
+        DestroyBackBuffer();
+        CreateBackBuffer();
+        break;
+    }
     case WM_DESTROY:
     {
         PostQuitMessage(0);
-        return 0;
+        break;
     }
     case WM_PAINT:
     {
-        PAINTSTRUCT paint;
-        HDC deviceContext = BeginPaint(hWnd, &paint);
-        PatBlt(
-            deviceContext,
-            paint.rcPaint.left,
-            paint.rcPaint.top,
-            paint.rcPaint.right - paint.rcPaint.left,
-            paint.rcPaint.bottom - paint.rcPaint.top,
-            BLACKNESS
-        );
-        EndPaint(hWnd, &paint);
+        break;
     }
-    default: return DefWindowProc(hWnd, uMsg, wParam, lParam);
     }
+    return DefWindowProc(hWnd, uMsg, wParam, lParam);
 }
 
 int CALLBACK WinMain(
@@ -34,6 +54,8 @@ int CALLBACK WinMain(
     _In_ LPSTR lpCmdLine,
     _In_ int nShowCmd)
 {
+    bool isRunning = true;
+
     WNDCLASS MainWindowClass = {};
     MainWindowClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
     MainWindowClass.lpfnWndProc = MainWindowProc;
@@ -42,7 +64,8 @@ int CALLBACK WinMain(
 
     if (RegisterClassW(&MainWindowClass))
     {
-        HWND hWindow = CreateWindowExW(
+        HWND window;
+        OS_CALL(window = CreateWindowExW(
             0,
             L"MainWindow",
             L"Software Rasterizer",
@@ -55,17 +78,42 @@ int CALLBACK WinMain(
             0,
             hInstance,
             0
-        );
+        ));
 
-        if (hWindow)
+        HDC screenContext;
+        OS_CALL(screenContext = GetDC(window));
+
+        RECT drawingArea;
+        OS_CALL(GetClientRect(window, &drawingArea));
+
+        while (isRunning)
         {
             MSG message;
-            while (GetMessageW(&message, 0, 0, 0))
+            if (PeekMessageW(&message, 0, 0, 0, PM_REMOVE))
             {
-                TranslateMessage(&message);
-                DispatchMessageW(&message);
+                if (message.message == WM_QUIT)
+                {
+                    OutputDebugString(L"Quit message reached loop.");
+                    isRunning = false;
+                }
+                else
+                {
+                    TranslateMessage(&message);
+                    DispatchMessageW(&message);
+                }
             }
+
+            //StretchDIBits(
+            //    screenContext,
+            //    drawingArea.left,
+            //    drawingArea.top,
+            //    drawingArea.right - drawingArea.left,
+            //    drawingArea.bottom - drawingArea.top,
+
+            //    )
         }
+
+        ReleaseDC(window, screenContext);
     }
 
     return 0;
