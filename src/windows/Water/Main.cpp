6618e8aacb15f4014897b3d1342285c1188572a0
Pavel Zavoiskikh <pavel.zavoiskikh@gmail.com>
2021-05-06 18:00:09 +0300

    Integrate starfield demo from old versions of code where I tried to do it on console.

--- src/windows/Water/Main.cpp
+++ src/windows/Water/Main.cpp
@@ -1,6 +1,14 @@
 #include <Windows.h>
 #include <stdint.h>
 #include <cassert>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <thread>
+#include <chrono>
+
+using namespace std;
 
 static uint32_t GameWidth = 800;
 static uint32_t GameHeight = 600;
@@ -50,6 +58,65 @@ void DrawPixel(float x, float y, Color color)
     finalColor |= blue << 0;
 
     backBuffer[j * GameWidth + i] = finalColor;
+}
+
+float star_speed = 0.01;
+int stars_count = 200;
+int fps = 25;
+
+vector<float> star_x(stars_count);
+vector<float> star_y(stars_count);
+vector<float> star_z(stars_count);
+
+float generate_random() {
+    return static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
+}
+
+void create_star(int i) {
+    star_x[i] = generate_random() * 2 - 1;
+    star_y[i] = generate_random() * 2 - 1;
+    star_z[i] = generate_random() + 0.1;
+}
+
+bool within_screen(float x, float y) {
+    return (-1 <= x && x <= 1) &&
+        (-1 <= y && y <= 1);
+}
+
+bool too_close(float z) {
+    return z <= 0;
+}
+
+void draw_stars(chrono::milliseconds delta) 
+{
+    for (uint32_t i = 0; i <= GameWidth; i++)
+        for (uint32_t j = 0; j <= GameHeight; j++)
+            DrawPixel(
+                2.0f / GameWidth * i - 1.0f,
+                2.0f / GameHeight * j - 1.0f,
+                { 0, 0, 0 }
+            );
+
+    auto delta_seconds = chrono::duration_cast<chrono::seconds>(delta);
+    for (int i = 0; i < stars_count; i++) {
+        star_z[i] -= star_speed;
+
+        float z = star_z[i];
+        if (too_close(z)) {
+            create_star(i);
+            continue;
+        }
+
+        float x = (star_x[i] / z);
+        float y = (star_y[i] / z);
+
+        if (!within_screen(x, y)) {
+            create_star(i);
+            continue;
+        }
+
+        DrawPixel(x, y, { 255, 255, 255 });
+    }
 }
 
 LRESULT CALLBACK MainWindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
@@ -116,10 +183,16 @@ int CALLBACK WinMain(
         BackBufferInfo.bmiHeader.biCompression = BI_RGB;
         NOT_FAILED(BackBuffer = VirtualAlloc(0, GameWidth * GameHeight * sizeof(uint32_t), MEM_COMMIT, PAGE_READWRITE), 0);
 
-        uint8_t offset = 0x00U;
-        int32_t direction = 1;
+        for (int i = 0; i < stars_count; i++) {
+            create_star(i);
+        }
+
+        auto frame_time = chrono::milliseconds(1000 / fps);
+
         while (isRunning)
         {
+            auto start = chrono::steady_clock::now();
+
             MSG message;
             if (PeekMessageW(&message, 0, 0, 0, PM_REMOVE))
             {
@@ -135,17 +208,7 @@ int CALLBACK WinMain(
                 }
             }
 
-            for (uint32_t i = 0; i <= GameWidth; i++)
-            for (uint32_t j = 0; j <= GameHeight; j++)
-                DrawPixel(
-                    2.0f / GameWidth * i - 1.0f,
-                    2.0f / GameHeight * j - 1.0f,
-                    { 
-                        static_cast<uint8_t>((static_cast<float>(i) / GameWidth) * 0xFF),
-                        offset,
-                        static_cast<uint8_t>((static_cast<float>(j) / GameHeight) * 0xFF)
-                    }
-                );
+            draw_stars(frame_time);
 
             StretchDIBits(
                 screenContext,
@@ -163,17 +226,12 @@ int CALLBACK WinMain(
                 SRCCOPY
             );
 
-            if (offset == 0xFFU)
-            {
-                direction = -1;
-            }
+            auto finish = chrono::steady_clock::now();
+            auto time_left = frame_time - (finish - start);
 
-            if (offset == 0x00U)
-            {
-                direction = 1;
+            if (time_left > 0ms) {
+                this_thread::sleep_for(time_left);
             }
-
-            offset += direction;
         }
 
         NOT_FAILED(VirtualFree(BackBuffer, 0, MEM_RELEASE), 0);
